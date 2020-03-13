7bc4b89ffdba9e28e67320f3585c6b50bdcb3516
Yarull Alvarez <yarullalvarez@hotmail.com>
2020-03-13 13:15:35 -0600

    first

---
+++ README.md
@@ -0,0 +1,16 @@
+# api_helpers
+
+#### Installation
+`composer require yarull/api_helpers`
+
+#### Add this line to your conf/app.php file
+
+`'JSONResponse' => Yarull\ApiHelpers\Facades\JSONResponse::class in config/app.php`
+
+##### Usage
+```
+
+
+```
+
+
---
+++ src/Facades/JSONResponse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Yarull\Facades\ApiHelpers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JSONResponse extends Model
+{
+
+  public static function ok($data){
+    return response()->json([
+      'status' => 'ok',
+      'data' => $data
+    ],200);
+  }
+
+  public static function err($err,$message)
+  {
+    abort(response()->json([
+      'status' => 'error',
+      'message' => $message,
+    ], $err));
+  }
+
+  public static function exist($obj,$response,$name=null,$message=null){
+    if(!$obj){
+      if(!$name){
+        $message = 'La búsqueda no arrojó resultados';
+      }
+      elseif (!$message) {
+        $message = 'La búsqueda de ' . $name . ' no arrojó resultados';
+      }
+      self::err(404,$message);
+    }
+  }
+
+}
